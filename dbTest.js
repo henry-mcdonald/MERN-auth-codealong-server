fd68ff7af6a06bcc710bdd51b39ca821878731ec
weston-bailey <wdbailey87@gmail.com>
2021-03-18 11:59:26 -0700

    add dbTest.js

--- dbTest.js
+++ dbTest.js
@@ -1,10 +1,52 @@
 // db tests to run
+// require models folder
+require('./models')
+// require the user model
+const User = require('./models/User.js')
 
-// CREATE a user
+const userTest = async () => {
+  try {
+    // CREATE a user
+    const newUser = new User({
+      name: 'bing',
+      email: 'bing@bang.com',
+      password: 'bingbang'
+    })
+  
+    await newUser.save()
+    console.log('newUser', newUser)
 
-// READ a user
+    // READ a user
+    const foundUser =  await User.findOne({
+      name: newUser.name
+    })
 
-// UPDATE a user
+    console.log('foundUser', foundUser)
 
-// DESTROY a user
+    // UPDATE a user
+    foundUser.name = 'bangBang'
 
+    await foundUser.save()
+
+    const findUserAgain = await User.findOne({
+      name: 'bangBang'
+    })
+
+    console.log('findUserAgain', findUserAgain)
+
+    // DESTROY a user
+    const deleteUser = await User.deleteOne({
+      name: 'bangBang'
+    })
+
+    console.log('deleteUser', deleteUser)
+
+    // we done
+    process.exit()
+  } catch (error) {
+    console.log(error)
+    process.exit()
+  }
+}
+
+userTest()
