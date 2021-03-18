76852c0b62c5eae70622346f3927d60dd754f6d0
weston-bailey <wdbailey87@gmail.com>
2021-03-18 10:48:51 -0700

    make server

---
+++ server.js
@@ -0,0 +1,30 @@
+// require packages
+const express = require('express')
+const rowdy = require('rowdy-logger')
+const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
+
+// config express app
+const app = express()
+const PORT = process.env.PORT || 3001
+const rowdyResults = rowdy.begin(app)
+// middleswares
+app.use(morgan('tiny'))
+app.use(cors())
+// request body parser
+// for html for POSTs
+// app.use(express.urlencoded({ extended: false }))
+// for plain JSON request bodies
+app.use(express.json())
+
+// GET / -- test index route
+app.get('/', (req, res) => {
+  res.json({ msg: 'hello world 👾' })
+})
+
+// tell express to listen on a port
+app.listen(PORT, () => {
+  rowdyResults.print()
+  console.log(`you are listening to the smooth sound of ${PORT} in the morning 🌊`)
+})
