fb526d6230cd90cb39237b505b21a4a1020181e5
weston-bailey <wdbailey87@gmail.com>
2021-03-18 14:42:02 -0700

    add login

--- controllers/api-v1/users.js
+++ controllers/api-v1/users.js
@@ -52,8 +52,39 @@ router.post('/register', async (req, res) => {
 })
 
 // POST /users/login -- validate crendentials
-router.post('/login', (req, res) => {
-  res.json({ msg: 'login a user' })
+router.post('/login', async (req, res) => {
+  try {
+    // try to find the user in db from the req.body
+    const foundUser = await User.findOne({
+      email: req.body.email
+    })
+
+    const noLoginMessage = 'Incorrect username or password'
+
+    // if the user is not found -- return with a failure status and a message (incorrect email)
+    if(!foundUser) return res.status(400).json({ msg: noLoginMessage })
+
+    // check the password from the db against the req.body
+    const matchPassword = await bcrypt.compare(req.body.password, foundUser.password)
+
+    // if the provided password doesn't match -- return with a failure status
+    if(!matchPassword) return res.status(400).json({ msg: noLoginMessage})
+
+    // create jwt token
+    const payload = {
+      name: foundUser.name,
+      email: foundUser.email,
+      id: foundUser.id
+    }
+
+    // sign the jwt token and send it back
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 60 * 60 })
+
+    res.json({ token })
+  } catch(error) {
+    console.log(error)
+    res.status(500).json({ msg: 'server error AHHHHHH!' })
+  }
 })
 
 // GET /auth-locked -- redirect if a bad token is found
